b5b46067a243a2480255b5f18b933dcc42c0cfc4
mengfangui <13273598220>
2022-06-18 13:23:50 +0800

    '前端Vue项目中使用SVG图标基础模板'

---
+++ src/components/SvgIcon/svgIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="svgIcon">
+    svgIcon页
+    <svg aria-hidden="true" class="svgClass">
+      <!-- <use :xlink:href="iconName" /> -->
+      <use xlink:href="#icon-water"></use>
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.svgClass {
+  color: pink;
+}
+.svgClass:hover {
+  color: #409eff;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    home页
+    <div class="box">
+      <svg aria-hidden="true" class="svgClass">
+        <!-- <use :xlink:href="iconName"  /> -->
+        <use xlink:href="#icon-water"></use>
+        <!-- 注意使用:<use xlink:href="#icon-XXX"></use>  XXX代表下载好的svg名称,修改之后就是<use xlink:href="#icon-water"></use>-->
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.svgClass{
+  color: rgba(240, 20, 130, 0.8);
+}
+
+.svgClass:hover{
+  fill: currentColor;
+  color: pink;
+}
+</style>
+
+
+
+
+
+
